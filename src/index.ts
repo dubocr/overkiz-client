6a26308019d0d81eac7a0eac61ce3716403997aa
Romain DUBOC <romain@duboc.pro>
2022-09-19 12:49:31 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import Command from './models/Command';
 import Device, { State } from './models/Device';
 import Execution, { ExecutionState } from './models/Execution';
 import Gateway from './models/Gateway';
+import LocalApiToken from './models/LocalApiToken';
 import Location from './models/Location';
 import Setup from './models/Setup';
 
-export { Client, Setup, Gateway, Device, State, Execution, ExecutionState, Command, Action, ActionGroup, Location };
+export { Client, Setup, Gateway, Device, State, Execution, ExecutionState, Command, Action, ActionGroup, Location, LocalApiToken };
